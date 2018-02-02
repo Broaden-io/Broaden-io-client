90b29a7994295ab6a2a617b7b1e3b5ccba5df8db
AlirieGray <caitlin.estrellita@gmail.com>
2018-02-02 10:53:44 -0800

    added header with links:

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Rubric from './components/Rubric';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
+          <Header />
           <Switch>
             <Route path="/" component={Dashboard} exact={true } />
             <Route path="/rubric" component={Rubric} />
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Header extends Component {
+  render() {
+    // TODO:add activeClassName for when the link is selected
+    return (
+      <header>
+
+        <NavLink to="/"exact={true}>Dashboard</NavLink>
+        <NavLink to="/rubric">Rubric</NavLink>
+      </header>
+    );
+  }
+}
+
+export default Header;
