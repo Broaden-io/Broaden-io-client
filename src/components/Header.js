90372a024a2e10404a12786cce8674a7b2c485da
AlirieGray <caitlin.estrellita@gmail.com>
2018-02-02 11:41:36 -0800

    added login variable

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,23 @@ import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
   render() {
+    var loggedIn = false;
     // TODO:add activeClassName for when the link is selected
     return (
       <header>
-        <NavLink to="/"exact={true}>Dashboard</NavLink>
-        <NavLink to="/rubric">Show Rubric</NavLink>
+        {loggedIn &&
+          <div>
+            <NavLink to="/"exact={true}>Dashboard</NavLink>
+            <NavLink to="/rubric">Show Rubric</NavLink>
+          </div>
+        }
+        {!loggedIn &&
+          <div>
+            Login      SignUp
+          </div>
+
+        }
+
       </header>
     );
   }
