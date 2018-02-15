74cf27d0bee7896faace28bb6de1a3eb1e0132ac
Jeffchiucp <jeffchiu2000@gmail.com>
2018-02-15 15:09:56 -0800

    added FrontEnd component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './components/Home'
-import SignUp from './components/SignUp'
-import Login from './components/Login'
+import { CookiesProvider, withCookies } from 'react-cookie';
+import Home from './components/Home';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 
 class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div className="wrapper">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={SignUp} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <CookiesProvider >
+        <BrowserRouter>
+          <div className="wrapper">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={SignUp} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </CookiesProvider>
     );
   }
 }
