e6d120076d876c10fd21c58629639269660d23e5
Shane Austrie <austrie@Shanes-MacBook-Pro.local>
2018-02-19 18:32:26 -0800

    Make code review changes for my last commit.

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,34 +1,24 @@
 import React, { Component } from 'react';
 
-// Example:
-// <Input name="email" type="email" label="Email Address"
-// required={true} validation="/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/" />
-
 class Input extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputText: "",
-      validated: true
-    }
-    this.setInputText = this.setInputText.bind(this);
-    this.checkValidation = this.checkValidation.bind(this);
+  const { label, required, name, type } = this.props;
+
+  state = {
+    inputText: "",
+    validated: true
   }
 
-  setInputText(e) {
+  setInputText = (e) => {
     e.preventDefault();
-    console.log(this.state.inputText)
     this.setState({
       inputText: e.target.value
-    }, () => {
-      this.checkValidation();
-    })
+    }, this.checkValidation())
   }
 
-  checkValidation() {
-    console.log("checking validation")
-    console.log(this.props.validation)
+  checkValidation = () => {
+    // console.log("checking validation")
+    // console.log(this.props.validation)
     // TODO
     // if (!this.props.validation) {
     //   return;
@@ -50,16 +40,12 @@ class Input extends Component {
   }
 
   render() {
-    let asterisk = "";
-    if (this.props.required) {
-      asterisk = "*";
-    }
 
     return(
       <div className="form-group label-floating is-empty">
-          <label className="control-label">{this.props.label}<small>{asterisk}</small></label>
-          <input className="form-control" name={this.props.name} type={this.props.type}
-          required={this.props.required} aria-required={this.props.required}
+          <label className="control-label">{label}<small>{required ? "*" : ""}</small></label>
+          <input className="form-control" name={name} type={type}
+          required={required} aria-required={required}
           onChange={this.setInputText} value={this.state.inputText}
           />
           <span className="material-input"></span>
