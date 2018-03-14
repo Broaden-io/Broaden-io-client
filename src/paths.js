5ff25766a54dd057c0b215d2b99ebf579b20f801
Tassos Lambrou <lambrou@gmail.com>
2018-03-14 00:11:26 -0700

    Updated paths file to new herokup app path

--- src/paths.js
+++ src/paths.js
@@ -1,6 +1,6 @@
 const serverPaths = {
   dev: 'http://localhost:8000',
-  prod: 'https://rubrics-dashboard.herokuapp.com'
+  prod: 'https://rubrics-server.herokuapp.com'
 }
 
 const serverPath = (process.env.NODE_ENV === 'development') ? serverPaths.dev : serverPaths.prod
