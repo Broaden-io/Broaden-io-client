0bb920dc262c40816aaa8ce5859066c1d45f9771
AlirieGray <caitlin.estrellita@gmail.com>
2018-03-15 18:12:55 -0700

    added show profile route

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,8 @@ class Profile extends Component {
     const user = {
       username: localStorage.getItem('username'),
       userId: localStorage.getItem('userId'),
+      email: localStorage.getItem('email'),
+      bio: localStorage.getItem('bio'),
       firstName: localStorage.getItem('firstName'),
       lastName: localStorage.getItem('lastName'),
       avatarURL: localStorage.getItem('avatarURL'),
@@ -14,6 +16,27 @@ class Profile extends Component {
       <div className="row">
         <div className="col-md-2"></div>
         <div className="col-md-8">
+          <div className="card">
+            <div className="card-content">
+              <h4 className="card-title"> My Profile </h4>
+              <hr />
+              <div col-sm-6>
+                <p> Username: <small> {user.username} </small> </p>
+              </div>
+              <div col-sm-6>
+                <p> Email: <small> {user.email} </small> </p>
+              </div>
+              <div col-sm-6>
+                <p> First Name: <small> {user.firstName !== 'null' ? user.firstName : <a className="text-info" href={`/${user.username}/edit`}> Click here to add a first name </a>} </small> </p>
+              </div>
+              <div col-sm-6>
+                <p> Last Name: <small> {user.lastName !== 'null' ? user.lastName : <a className="text-info" href={`/${user.username}/edit`}> Click here to add a last name </a>} </small> </p>
+              </div>
+              <div col-sm-6>
+                <p> Bio: <small> {user.bio !== 'null' ? user.bio : <a className="text-info" href={`/${user.username}/edit`}> Click here to add a bio </a>} </small> </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
