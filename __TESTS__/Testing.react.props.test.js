b769b09d6741dfdaf5463a4ec9acdd8d686de902
Jeff Chiu <jeffchiu2000@gmail.com>
2018-03-30 14:02:06 -0700

    adding the react props template jest testing

---
+++ __TESTS__/Testing.react.props.test.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+// import { Link } from 'react-router-dom';
+//
+// test('Link matches snapshot', () => {
+//   const component = renderer.create(
+//     <StaticRouter location="someLocation" context={context}>
+//       <Link to="#" />
+//     </StaticRouter>
+//   );
+//
+//   let tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
+
+
+describe('it is just a test', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
