fe733f0b82e63080b6312c3ff0bb8e156262beba
Tassos Lambrou <lambrou@gmail.com>
2018-04-12 01:26:10 -0700

    checkbox independent of collapsible

--- src/components/Criterion.js
+++ src/components/Criterion.js
@@ -8,11 +8,16 @@ import { presets } from 'react-motion'
 
 class Criterion extends Component {
   state = {
-    isOpen: false
+    isOpen: false,
+    isChecked: false
   }
 
-  toggle() {
+  toggleOpen() {
     this.setState({isOpen: !this.state.isOpen})
+  }
+
+  toggleCheck() {
+    this.setState({isChecked: !this.state.isChecked})
   }
 
   render() {
@@ -21,25 +26,34 @@ class Criterion extends Component {
     return(
       <ul className="list-group">
         <li className="list-group-item">
-          <div onClick={this.toggle.bind(this)}>
+          <div className="checkbox" style={{display: `inline`}}>
+            <label style={{display: `inline`}}>
+              <input
+                type="checkbox"
+                onClick={this.toggleCheck.bind(this)}
+                value={this.isChecked}
+                name="optionsCheckboxes"
+                style={{display: `inline`}}/>
+            </label>
+          </div>
+          <span onClick={this.toggleOpen.bind(this)}>
             <span className="btn btn-info btn-sm btn-round pull-right">
               <strong>
                 {level === 1 ? `${level} pt` : `${level} pts`}
               </strong>
             </span>
-            <div className="checkbox" style={{display: `inline`}}>
-              <label style={{display: `inline`}}>
-                <input type="checkbox" name="optionsCheckboxes" style={{display: `inline`}}/>
-              </label>
-            </div>
+
             <h3 className="list-group-item-heading" style={{display: `inline`}}>&nbsp; <small>{text}</small></h3>
-          <br/>
-          <button className="btn btn-sm btn-primary btn-simple">
-                              <i className="material-icons">add</i>
-                              &nbsp; See learning actions to level up this skill
-                          <div className="ripple-container"></div></button>          </div>
+            <br/>
+            <button className="btn btn-sm btn-primary btn-simple">
+              <i className="material-icons">add</i>
+              &nbsp; See learning actions to level up this skill
+              <div className="ripple-container">
+              </div>
+            </button>
+          </span>
           <Collapse isOpened={isOpen} springConfig={{stiffness: 170, damping: 26}}>
-              <button className="btn btn-default btn-block">Add a new resource</button>
+            <button className="btn btn-default btn-block">Add a new resource</button>
           </Collapse>
         </li>
       </ul>
