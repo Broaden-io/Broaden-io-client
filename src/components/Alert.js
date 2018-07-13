a34bd6e8c47c58a921b1e085a8c650e771e00e41
Jeff Chiu <jeffchiu2000@gmail.com>
2018-07-13 10:18:44 -0700

    add alert notification

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,21 +2,21 @@ import { toast } from 'react-toastify';
 
 export const Alert = (type) => {
   switch (type) {
-    case 'loginError':
-      toast('Could not login', {
-        autoClose: 3000,
-        className: "alert alert-rose",
-        render: '<h2><strong>Could not login</strong></h2>'
-      });
-      break
-    case 'signupError':
-      toast('Could not signup', {
-        autoClose: 3000,
-        className: "alert alert-rose",
-        render: '<h2><strong>Could not login</strong></h2>'
-      });
-      break
-    default:
-      break
+  case 'loginError':
+    toast('Could not login', {
+      autoClose: 3000,
+      className: "alert alert-rose",
+      render: '<h2><strong>Could not login</strong></h2>'
+    });
+    break
+  case 'signupError':
+    toast('Could not signup, Username must be greater than or equal to 5 characters', {
+      autoClose: 3000,
+      className: "alert alert-rose",
+      render: '<h2><strong>Could not signup, Username must be greater than or equal to 5 characters</strong></h2>'
+    });
+    break
+  default:
+    break
   }
 }
